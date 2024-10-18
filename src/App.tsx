8ace121aa7726ac0219222bf3518447190ea73cd
Wa Wa <wawa@xenoptics.com>
2024-10-18 10:12:18 +0700

    some new logos from designer and some modification for route adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import IntranetUI from './intranet/IntranetUI';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/intranet" />} />
         <Route path="/intranet/*" element={<IntranetUI />} />
         {/* Add more routes here if needed */}
       </Routes>
